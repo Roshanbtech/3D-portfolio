89a1a06097390786c749436fc9e73cf151cdcc18
Roshan Reji <roshanreji987@gmail.com>
2024-11-09 16:16:35 +0530

    nav comp add change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { Home, About, Projects, Contact } from './pages';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 
 
 const App = () => {

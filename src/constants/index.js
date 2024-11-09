dc62c49bdc767c8ad0000771845c2e04f78cc918
Roshan Reji <roshanreji987@gmail.com>
2024-11-09 16:08:23 +0530

    portfolio initial commit full

---
+++ src/constants/index.js
@@ -0,0 +1,206 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    
+    
+
+} from "../assets/icons";
+
+export const skills = [
+    
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    },
+];
+
+export const experiences = [
+    {
+      title: "Web Developer Intern (MERN)",
+      company_name: "Brototype, Trivandrum",
+      icon: starbucks, // Replace with an appropriate icon if available
+      iconBg: "#accbe1",
+      date: "2023 - 2024",
+      points: [
+        "Internship focusing on hands-on MERN stack development.",
+        "Gained practical experience in full-stack development projects.",
+      ],
+    },
+    {
+      title: "Bachelor of Technology in Electronics and Communication Engineering",
+      company_name: "APJ Abdul Kalam Technological University, Thiruvananthapuram, Kerala",
+      icon: meta, // Replace with an appropriate icon if available
+      iconBg: "#fbc3bc",
+      date: "2023",
+      points: [
+        "Minor Degree in Computer Science Engineering.",
+        "College: College of Engineering, Chengannur.",
+        "CGPA: 7.7/10.",
+      ],
+    },
+    {
+      title: "Higher Secondary Education",
+      company_name: "Kerala Board of Higher Secondary Examination, Thiruvananthapuram, Kerala",
+      icon: shopify, // Replace with an appropriate icon if available
+      iconBg: "#b7e4c7",
+      date: "2019",
+      points: [
+        "School: Marthoma Higher Secondary School, Venmony, Chengannur.",
+        "Percentage: 95.4%.",
+      ],
+    },
+    {
+      title: "High School Education (SSLC)",
+      company_name: "Kerala Board of Public Examinations, Kerala",
+      icon: tesla, // Replace with an appropriate icon if available
+      iconBg: "#a2d2ff",
+      date: "2017",
+      points: [
+        "School: Marthoma Higher Secondary School, Venmony, Chengannur.",
+        "Percentage: 98%.",
+      ],
+    },
+  ];
+  
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/YourGitHubUsername',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-pink',
+        name: 'project1',
+        description: 'description1', 
+        link: 'url1',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'project2',
+        description: 'description2',
+        link: 'url2',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'project3',
+        description: 'description3',
+        link: 'url3',
+    },
+    {
+        iconUrl:snapgram,
+        theme: 'btn-back-pink',
+        name: 'project4',
+        description: 'description4',
+        link: 'url4',
+    },
+    {
+        iconsUrl: estate,
+        theme: 'btn-back-black',
+        name: 'project5',
+        description: 'description5',
+        link: 'url5',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'project6',
+        description: 'description6',
+        link: 'url6',
+    }
+];
